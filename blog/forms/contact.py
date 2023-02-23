8a7a7015133a770a3be30b545b47f48fb01ebf80
Kaan Ince <kaaniince@outlook.com>
2023-02-23 17:16:05 +0300

    feat:contact form is added

---
+++ blog/forms/contact.py
@@ -0,0 +1,6 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    email = forms.EmailField(max_length=100)
+    name_surname = forms.CharField(max_length=25)
+    message=forms.CharField(widget=forms.Textarea)
--- blog/views/contact.py
+++ blog/views/contact.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from blog.forms import ContactForm
+from blog.models import ContactModel
 def contact(request):
+    form=ContactForm()
+    if request.method=='POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            contact=ContactModel()
+            contact.email=form.cleaned_data['email']
+            contact.name_surname=form.cleaned_data['name_surname']
+            contact.message=form.cleaned_data['message']
+            contact.save()
+            return redirect('homepage')
+        else:
+            print()
     context={
-        'key':'baslik icerik naber'
+        'form':form
     }
     return render(request,'pages/contact.html',context=context)
